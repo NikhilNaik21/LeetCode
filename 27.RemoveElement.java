deae037933643de8c024ea55174c914d2599c782
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-10-11 00:06:51 +0530

    Create 27.RemoveElement.java

---
+++ 27.RemoveElement.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        Arrays.sort(nums);
+     
+        int count =0;
+        ArrayList<Integer> arr =  new ArrayList<>();
+                for(int i =0;i<nums.length;i++)
+                {
+                    if(nums[i]!=val)
+                    {
+                        nums[count]=nums[i];
+                        count++;
+                    }
+                }
+                return count;
+
+            }
+        }
+        
+ 
